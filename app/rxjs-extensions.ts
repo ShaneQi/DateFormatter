1816f69212599ae0c2deaeb571447a0569f05e35
Shane Qi <qizengtai@gmail.com>
2016-12-05 10:37:23 -0600

    Update rxjs-extensions.ts

--- app/rxjs-extensions.ts
+++ app/rxjs-extensions.ts
@@ -1,10 +1,3 @@
 import 'rxjs/add/operator/debounceTime';
 import 'rxjs/add/operator/distinctUntilChanged';
 import 'rxjs/add/operator/map';
-
-/*
-Copyright 2016 Google Inc. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at http://angular.io/license
-*/
-
