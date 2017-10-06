0be9ce8b4986b45f056c698eb1e6a5ed87be4599
Shane Qi <qizengtai@gmail.com>
2017-10-06 17:16:17 -0500

    Added timezone support.

--- app.js
+++ app.js
@@ -6,13 +6,14 @@ angular.module("DateFormatterPlayground", [])
 
         this.format = "MMM. dd, yyyy HH:mm z"
         this.dateStringInput = "Dec. 06, 1991 09:41 CDT"
+        this.timeZone = "GMT"
 
         this.$onInit = function () {
             this.post();
         }
 
         this.post = function () {
-            $http.post('https://server.shaneqi.com/dateformatter', { format: this.format, date_string: this.dateStringInput }).then(function (response) {
+            $http.post('https://server.shaneqi.com/dateformatter', { format: this.format, date_string: this.dateStringInput, time_zone: this.timeZone }).then(function (response) {
                 $scope.dateString = response.data.date_string;
                 $scope.dateValue = response.data.date_value;
             });
